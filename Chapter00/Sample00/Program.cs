872c72a2745517beb27c580b3210a4805f66bc5b
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-04-16 11:57:13 +0900

    10回出力します

--- Chapter00/Sample00/Program.cs
+++ Chapter00/Sample00/Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+           
             {
-                Console.WriteLine("Hello, World!");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello, World!");
+                }
             }
         }
     }
