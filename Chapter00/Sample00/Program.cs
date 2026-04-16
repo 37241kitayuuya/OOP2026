290835307cf30d89cc1545b9c0cc8fe6674e9a52
infosys <infosys@C4224>
2026-04-16 10:56:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample00/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Chapter00/sample0415/sample0415/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            //入力(配列へ格納）
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "]:");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            //集計（配列の値を集計）
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+            //出力（配列の値を出力）
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("array[" + i + "]:" + array[i]);
+                for (int j = 0; j < array[i]; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                //合計を出力
+                Console.WriteLine("合計値:" +array.Where(n=>n%2==0).Sum());
+            }
+
+            void asOut(int num)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();//改行
+
+            }
+        }
+    }
+}
