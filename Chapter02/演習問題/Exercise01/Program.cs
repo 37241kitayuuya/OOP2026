a4c8d7f171993440b334b850a66bae76d38bd78c
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-05-11 11:31:12 +0900

    2.1.4 完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -16,7 +16,13 @@ namespace Exercise01 {
         }
 
         private static void PrintSongs(Song[] songs) {
-            
+            foreach(var song in songs) {
+                Console.WriteLine($"{song.Title},{song.ArtistName}{song.Length/60+":"
+                    +song.Length%60}");
+            }
+
+
+
         }
     }
 }
