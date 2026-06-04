7375fdddda160bf0362bdabbd9a05cb1bbcfdfe3
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-04 10:20:47 +0900

    完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -10,7 +10,7 @@ namespace Exercise01 {
         //プロパティ（ｐ114）
         public int Year { get; init; }
         public int Month { get; init; }
-        public YearMonth (int year,int month) {
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
@@ -20,12 +20,18 @@ namespace Exercise01 {
         public bool Is21Century => 2001 <= Year && Year <= 2100;
         //5.1.3
         public YearMonth Add0neMonth() {
+            {
+                if (Month == 12) {
+                    return new YearMonth(Year + 1, 1);
+                }
 
+                return new YearMonth(Year, Month + 1);
+            }
         }
 
         //5.1.4
-        public override string ToString() {
+        public override string ToString() => $"{Year}年{Month}月";
             return base.ToString();
         }
     }
-}
+
