87284a137ea2b211068d953847e29e8955095c5c
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-19 11:29:42 +0900

    7.1.5まで完成

---
+++ Chapter07/section01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+
+            var books = Books.GetBooks();
+            var priceAverage = books.Average(x);
+        }
+    }
+}
