d4e36ce65c7f1be97d22795fd035d457ac7a42fd
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-22 11:53:37 +0900

    設問１＿提出

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -9,23 +9,33 @@
 
         //メソッドの概要：
         private static IEnumerable<Student> ReadScore(string filePath) {
-            var sales = new List<Student>();
+            var students = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
-
-
-
-
-
-
-
-            return sales;
+            foreach (var line in lines) {
+                var items = line.Split(',');//カンマ区切りで分割
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.
+                    Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
+            foreach (var student in _score) {
+                //すでに店舗名が辞書のキーに登録されているか？
+                if (dict.ContainsKey(student.Name))
+                    //登録されている場合
+                    dict[student.Name] += Student.score;//売上を足しこみ
+                else
+                    //未登録の場合
+                    dict[student.Name] = Student.score;//新規に売り上げを登録
+            }
 
 
 
--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,10 +1,12 @@
 ﻿namespace Test01_01 {
     public class Student {
-            public string Name { get;private set; } = string.Empty;
+        internal static int score;
+
+        public string Name { get;private set; } = string.Empty;
             
             public string Subject { get;private set; } = string.Empty;
 
-        public int Score { get; private set; } 
+            public int Score { get; private set; } 
     }
    
 
