091587f366c4c096d14ad4dd0533e95db227c724
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-22 11:30:12 +0900

    設問１＿studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,12 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
+            public string Name { get;private set; } = string.Empty;
+            
+            public string Subject { get;private set; } = string.Empty;
 
+        public int Score { get; private set; } 
+    }
+   
 
     }
-}
+
