97cf1f50ffeee91a8941ef089658aae1437105c9
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-29 14:15:41 +0900

    完成

--- Chapter08/Section01/Program.cs
+++ Chapter08/Section01/Program.cs
@@ -19,7 +19,11 @@
                 Console.Write("県庁所在地:");
                 prefCaptalLocation = Console.ReadLine();
                 Console.WriteLine();
-
+                //既に登録済みかの判断
+                if (pre0fficeDict.ContainsKey(pref)) {
+                    Console.WriteLine("上書きしますか？(Y/N)");
+                    if (Console.ReadLine() == "N") ;
+                }
                 if (prefCaptalLocation == null) break;
 
                 //県庁所在地登録処理
@@ -54,24 +58,30 @@
             Console.WriteLine("2:検索");
             Console.WriteLine("9:終了");
             Console.Write(">");
-            int menu;
-            if (int.TryParse(Console.ReadLine(), out menu)) {
-                return menu;
-            }
             //メニュー番号を入力させて呼び出し元へ返却
-            return 0; 
+            return int.Parse(Console.ReadLine());
+            //int menu;
+            //if (int.TryParse(Console.ReadLine(), out menu)) {
+           // }
+            //return 0; 
         }
 
         //一覧表示処理
         private static void allDisp() {
-
             //コレクションの中身をすべて出力
-
+            foreach(var p in pre0fficeDict) {
+                Console.WriteLine($"{p.Key}の県庁所在地は{p.Value}です。");
+            }
         }
         //検索処理
         private static void searchPrefCaptalLocation() {
             Console.Write("都道府県");
             string? searchPref = Console.ReadLine();
+            if (searchPref is null) return;
+            //検索結果を表示
+            if (pre0fficeDict.ContainsKey(searchPref)) {
+                Console.WriteLine(searchPref + "の県庁所在地は" + pre0fficeDict[searchPref] + "です");
+            }
         }
     }
     }
