379f849fcbb2721a461e8ed709b5ce8271f5b63e
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-06-30 14:43:33 +0900

    問題8_1 完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Exercise01 {
     internal class Program {
+        private static object count;
+
         static void Main(string[] args) {
             var text = "Cozy lummox gives smart squid who asks for job pen";
 
@@ -9,8 +11,26 @@
         }
 
         private static void Exercise1(string text) {
+            //var dict = new Dictionary<char,int>();
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+            foreach (char c in text) {
+                if (char.IsLetter(c)) // アルファベットのみ対象
+                {
+                    char ch = char.ToUpper(c); // 大文字・小文字を区別しない
+
+                    if (dict.ContainsKey(ch))
+                        dict[ch]++;
+                    else
+                        dict[ch] = 1;
+                }
+            }
+
+            foreach (var item in dict.OrderBy(x =>x.Key)) {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
 
         }
+
         private static void Exercise2(string text) {
 
         }
