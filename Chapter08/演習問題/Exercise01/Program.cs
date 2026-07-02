019b51ba5bd98e1e6ab889f48f1035c7e9ee85a9
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-07-02 14:21:52 +0900

    完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -11,13 +11,10 @@
         }
 
         private static void Exercise1(string text) {
-            //var dict = new Dictionary<char,int>();
-            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
-            foreach (char c in text) {
-                if (char.IsLetter(c)) // アルファベットのみ対象
+            var dict = new Dictionary<char,int>();
+            foreach (char ch in text.ToUpper() ){
+                if ('A' <= ch && ch <= 'Z') // アルファベットのみ対象
                 {
-                    char ch = char.ToUpper(c); // 大文字・小文字を区別しない
-
                     if (dict.ContainsKey(ch))
                         dict[ch]++;
                     else
@@ -32,7 +29,23 @@
         }
 
         private static void Exercise2(string text) {
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+            foreach (char ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') // アルファベットのみ対象
+                {
+                    if (dict.ContainsKey(ch))
+                        dict[ch]++;
+                    else
+                        dict[ch] = 1;
+                }
+            }
+
+            foreach (var item in dict.OrderBy(x => x.Key)) {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
 
         }
+
+    }
     }
 }
