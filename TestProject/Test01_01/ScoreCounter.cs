7d6c7469192e6543b137a5723d772c855abe3036
37241kitayuuya <oibc37241@ojs.ac.jp>
2026-07-07 21:20:01 +0900

    訂正済み
    Refactor ScoreCounter to use sales list instead of students list for score tracking.

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -1,44 +1,47 @@
 ﻿namespace Test01_01 {
     public class ScoreCounter {
         private IEnumerable<Student> _score;
-
+ 
         // コンストラクタ
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
         }
-
+ 
         //メソッドの概要：
         private static IEnumerable<Student> ReadScore(string filePath) {
-            var students = new List<Student>();
+            var sales = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            foreach (var line in lines) {
-                var items = line.Split(',');//カンマ区切りで分割
-                var student = new Student {
+            foreach (string line in lines) {
+                var items = line.Split(',');   //カンマ区切りで分割
+                var sale = new Student {
                     Name = items[0],
                     Subject = items[1],
-                    Score = int.
-                    Parse(items[2])
+                    Score = int.Parse(items[2]),
                 };
-                students.Add(student);
+                sales.Add(sale);
             }
-            return students;
+            return sales;
         }
-
+ 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-            foreach (var student in _score) {
-                //すでに店舗名が辞書のキーに登録されているか？
-                if (dict.ContainsKey(student.Name))
-                    //登録されている場合
-                    dict[student.Name] += Student.score;//売上を足しこみ
+ 
+            foreach (var sale in _score) {
+                
+                if (dict.ContainsKey(sale.Name))
+                   
+                    dict[sale.Name] += sale.Score;
                 else
-                    //未登録の場合
-                    dict[student.Name] = Student.score;//新規に売り上げを登録
+                   
+                    dict[sale.Name] = sale.Score;  
             }
-
-
-
+            return dict;
+        }
+    }
+}
+ 
+ 
 
 
             return dict;
