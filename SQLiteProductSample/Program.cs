ed79fc9494f189e26129f0a6eb12ff5c52f8024e
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-08-31 14:42:47 +0900

    DB Browserまで

--- SQLiteProductSample/Program.cs
+++ SQLiteProductSample/Program.cs
@@ -9,7 +9,7 @@ internal static class Program
 
         try
         {
-            Database.Initialize();
+            Database.Intializa();
             Application.Run(new Form1());
         }
         catch (Exception ex)
