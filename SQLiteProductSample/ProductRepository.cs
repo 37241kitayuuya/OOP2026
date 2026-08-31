b2aa5d62e3048773ab758b9c199b85806bcd82f5
優弥 喜多 <oibc37241@ojs.ac.jp>
2026-08-31 15:31:31 +0900

    プロダクトリポジトリ完成

--- SQLiteProductSample/ProductRepository.cs
+++ SQLiteProductSample/ProductRepository.cs
@@ -2,7 +2,41 @@ using Microsoft.Data.Sqlite;
 
 namespace SQLiteProductSample;
 
-public class ProductRepository
-{
-    
+//Productsテーブルに対するDB操作をまとめたクラス
+
+public class ProductRepository {
+    public List<Product> GetAll() {
+
+        var products = new List<Product>();
+
+        using var connection = Database.GetConnection();
+
+        connection.Open();
+
+
+        using var command = connection.CreateCommand();
+
+        command.CommandText =
+            """
+            SELECT Id Name. Price
+            FROM Products
+            ORDER BY Id;
+            """;
+
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read()) {
+            products.Add(new Product {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                price = reader.GetInt32(2)
+
+            });
+
+        }
+        return products;
+    }
+
 }
+
+
